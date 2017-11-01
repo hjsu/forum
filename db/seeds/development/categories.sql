9c87222989279e940d98f17fc1fd84a5591bb099
Harshvardhan Sukthankar <hsukthankar@gmail.com>
2017-11-01 03:34:14 +0000

    moved seeds to .sql files

---
+++ db/seeds/development/categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (id, name, description) VALUES
+    (1, 'Discussion', 'General Discussion'),
+    (2, 'Engineering', 'Forums Engineering')
+;
---
+++ db/seeds/development/forums.sql
@@ -0,0 +1,6 @@
+INSERT INTO FORUMS (id, category_id, parent_forum, name) VALUES
+    (1,1,null,'Faculty Discussion'),
+    (2,1,null,'Admissions'),
+    (3,1,null,'Off-Topic'),
+    (4,1,null,'Bugs')
+;
