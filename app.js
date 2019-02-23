835601528d005e10719d9b17a00411853d3f02b1
Harshvardhan Sukthankar <hsukthankar@gmail.com>
2019-02-23 17:06:49 -0500

    serving correctly

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ var app = express();
 
 // view engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
 
 router.get('/', function(req, res, next) {
   res.render('index', {
