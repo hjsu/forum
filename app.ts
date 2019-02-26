528806dc2d1d678d22a88c1c45d7a5a0890d4073
Harshvardhan Sukthankar <hsukthankar@gmail.com>
2019-02-25 22:17:02 -0500

    fin

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
 var path = require('path');
@@ -35,40 +36,23 @@ router.get('/topics/:id', function(req, res, next) {
 });
 app.use('/', router);
 
-interface Error{
-    status?: number;
-}
-
+// error handlers
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
+
 
 
 var debug = require('debug')('firstapp:server');
